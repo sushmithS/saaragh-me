6c2b518d23e1e0a842f21f851bc152bc35197655
Sushmith Banoth <107675589+sushmithS@users.noreply.github.com>
2022-06-22 09:30:31 +0530

    Update emp_1.py

--- emp_1.py
+++ emp_1.py
@@ -1,3 +1,4 @@
+#Task 1 to get Hello world
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -12,6 +13,4 @@ def just_start():
 def create_emp_1(first_name:str,last_name:str,experience_in_months:int,city:str,CTC:int,emp_age:str,Contact_no:str,):
     return {first_name,last_name,experience_in_months,city,CTC, emp_age,Contact_no}
 
-
-#empoyee id - (should be generated - first employee -- A2022-001, next employee - A2022-002 - this should happen automatically) - you should NOT get from user
 
