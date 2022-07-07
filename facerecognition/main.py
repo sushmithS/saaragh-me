53fe829f35cc6dfb4a6775163047b094ef9f6251
Sushmith Banoth <107675589+sushmithS@users.noreply.github.com>
2022-07-07 15:03:14 +0530

    updated on 7july 2022
    1,upload image
    2.can see the response image
    3.Coverting color image  to greyscale

--- facerecognition/main.py
+++ facerecognition/main.py
@@ -1,46 +1,36 @@
-import os
+
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image,ImageFile
+from fastapi.responses import JSONResponse
+from typing import List
+from werkzeug.utils import secure_filename
+import logging
 import cv2
-from fastapi import FastAPI,UploadFile, File,responses
-#responses -for getting image as response
-from deta import Drive
-from fastapi.responses import FileResponse
-app=FastAPI()
-path= "/saaragh_sushmithAPI/facerecognition"
-#deta=Deta("")#configure your deta project
-#drive=deta.Drive("images")#acess to your drive
+import os
+import shutil
+import numpy as np
 
 
-@app.get("/")
-def index():
-    return {"hello"}
+app= FastAPI()
 
-@app.get("/hp")
-def hp():
-    return FileResponse("HP.jpg")
 
-@app.get("/hp/Grey_scale")
-def grey_image():
-    for image in grey_image():
-        grey_image=cv2.cvtColor("HP.jpg", cv2.COLOR_BGR2GRAY)
-    return grey_image
 
-#@app.get("/hp_and etc")
-#def HP():
-   #file_path=os.path.join(path,"saaragh_sushmithAPI/facerecognition/HP.jpg")
-   # if os.path.exists(file_path):
-   #     return FileResponse(file_path)
-  #  return {"error":"file not found "}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
 
-#@app.post("/single_image")
-#def image(file: UploadFile = File(...)):
- #   return files.put(file.filename,file.file)
+@app.post("/Upload")
+def upload_image(file:UploadFile=File(...)):
+    with open(f'{file.filename}',"wb") as buffer:
+            shutil.copyfileobj(file.file,buffer)
+    return {"file_name":file.filename}
 
-#@app.get("/")
-#def list_files():
-  #  return files.list()
-#
-#@app.get("/{name}")
-#def serve(name):
-   # img=files.get(name)
-    #ext =name.split(".")[1]
-   # return responses.StreamingResponse(img.iter_chunks(),media_type=f"images/{ext}")
+
+@app.post("/Grey_image")
+def grey_image(file:UploadFile=File):
+    filename=await get_uploaded_file(file)
+    img =Image.open(filename)
+    imgGray=img.convert('L')
+    imgGray.save('Grey_Image.jpg')
+    return FileResponse('Grey_Image.jpg')
+
