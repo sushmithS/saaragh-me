e1ded8c26ac643808b12b77588cadc3bc123dd91
Sushmith Banoth <107675589+sushmithS@users.noreply.github.com>
2022-07-08 14:08:08 +0530

    Update main.py

--- facerecognition/main.py
+++ facerecognition/main.py
@@ -1,7 +1,7 @@
-
 from fastapi import FastAPI, UploadFile, File
 from PIL import Image,ImageFile
 from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse
 from typing import List
 from werkzeug.utils import secure_filename
 import logging
@@ -13,24 +13,39 @@ import numpy as np
 
 app= FastAPI()
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(message)s'
+    )
+logger=logging.getLogger()
 
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+async def get_uploaded_file(file):
+    logger.info("File Uploading ...")
+    try:
+        filename = secure_filename(file.filename)
+        with open(filename,"wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        logger.info(f"filename:{filename}")
+    except:
+        filename == ''
+        logger.error("No file selected for uploading.")
+        resp=JSONResponse(content={'error_msg':'no file selected for uploading'})
+        resp.status_code=200
+        return resp
+    return filename
 
 @app.post("/Upload")
 def upload_image(file:UploadFile=File(...)):
     with open(f'{file.filename}',"wb") as buffer:
             shutil.copyfileobj(file.file,buffer)
-    return {"file_name":file.filename}
+    return FileResponse(file.filename)
 
 
-@app.post("/Grey_image")
-def grey_image(file:UploadFile=File):
-    filename=await get_uploaded_file(file)
-    img =Image.open(filename)
-    imgGray=img.convert('L')
-    imgGray.save('Grey_Image.jpg')
-    return FileResponse('Grey_Image.jpg')
-
+@app.post("/grey_file/")
+async def create_upload_file(file:UploadFile):
+    filename= await get_uploaded_file(file)
+    img = Image.open(filename)
+    imgGray =img.convert('L')
+    imgGray.save('test_gray.jpg')
+    return FileResponse("test_gray.jpg")
